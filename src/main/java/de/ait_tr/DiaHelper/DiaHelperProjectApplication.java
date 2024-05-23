45a8c7908208104deb4d6f4bb125254c723b674d
Garfield <demyanovaya@gmail.com>
2024-05-23 13:41:34 +0200

    git first

---
+++ src/main/java/de/ait_tr/DiaHelper/DiaHelperProjectApplication.java
@@ -0,0 +1,13 @@
+package de.ait_tr.DiaHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaHelperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaHelperProjectApplication.class, args);
+	}
+
+}
