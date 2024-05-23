6d2fa03dda3015f28186c726413f2cf526f872d0
Garfield <demyanovaya@gmail.com>
2024-05-23 16:45:08 +0200

    first commit

--- src/main/java/de/ait_tr/DiaHelper/DiaHelperProjectApplication.java
+++ src/main/java/de/ait_tr/DiaHelper/DiaHelperProjectApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiaHelperProjectApplication {
-
-	//commit
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiaHelperProjectApplication.class, args);
 	}
-
 }
