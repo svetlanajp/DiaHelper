1fb426f6b48071edabd4790d1976e0fd0203e0b4
Yadviha <demyanovaya@gmail.com>
2024-06-14 10:41:15 +0200

    prepare for deploy

---
+++ src/main/java/de/ait_tr/DiaHelper/config/AppConfig.java
@@ -0,0 +1,34 @@
+package de.ait_tr.DiaHelper.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public AmazonS3 digitalOceanClient(DOProperties properties) {
+
+        AWSCredentials credentials = new BasicAWSCredentials(
+                properties.getAccessKey(),
+                properties.getSecretKey());
+
+        AwsClientBuilder.EndpointConfiguration endpointConfig =
+                new AwsClientBuilder.EndpointConfiguration(
+                        properties.getEndpoint(),
+                        properties.getRegion());
+
+        AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withEndpointConfiguration(endpointConfig);
+
+        return clientBuilder.build();
+    }
+}
